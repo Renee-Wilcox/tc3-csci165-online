7974c7d013e39b63482ba51283cd1fddd5aad4c7
Renee-Wilcox <RLW018@mymail.tc3.edu>
2020-01-29 07:56:09 -0500

    lab install java

---
+++ module-1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+	//starting main method
+	public static void main(String[] args){
+
+		System.out.println("Hello World!"); //print message
+} //end main
+} //end class
---
+++ module-1/HelloWorld2.java
@@ -0,0 +1,9 @@
+public class HelloWorld2{
+
+	//starting main method
+	public static void main(String[] args){
+
+		for (String s : args)
+		System.out.println("String argument: " + s); //print message
+} //end main
+} //end class
