d5e76e803bdf16cc1beaf108195bbbf4e68e5915
Renee-Wilcox <RLW018@mymail.tc3.edu>
2020-02-12 20:25:43 -0500

    submission lab module 2

--- module-2/lab/Tickets.java
+++ module-2/lab/Tickets.java
@@ -1,33 +1,10 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException; 
 public class Tickets{
 
     public static void main(String[] args){
-
-        /* 
-            The following code steps you through the String processing
-            and math neccessary to determine if a single ticket number is valid.
-            Your job with this code is to:
-
-            ~   Closely study the code. Reverse engineer it. Use the API for guidance
-                to understand the classes and methods that are being used.
-            ~   Add comments for each statement, describing in detail what the line is doing
-                I want to know DETAILS, not generalizations. RESEARCH!
-            ~   When you have completed that, add code to complete the following
-
-                1) Using a Scanner, open the file: tickets.txt
-                2) Using a while(hasNext) loop, read each ticket number
-                3) Run the ticket number through the provided math and String processing
-                4) If the ticket number is valid write it to a new file called: valid_tix.txt
-                5) Ignore the invalid ticket numbers
-
-            ~   Additional code must also be commented. But I am more interested in why you are doing
-                something, not the details of how.
-
-            Check your work: There are 101 valid ticket numbers among the 1000 provided ticket numbers
-                             These people make mistakes!!!
-
-            Submit only Java source code files. Also submit valid_tix.txt   
-
-        */
 
         String  ticket  = "123454";                            
         String  last = ticket.substring(ticket.length() - 1);                                                   
@@ -38,5 +15,46 @@ public class Tickets{
         boolean validity = remainder == last_digit; 
         String  format = "Original Ticket #: %s\nLast Digit: %d\nReduced Ticket #: %d\nRemainder: %d\nValidity: %b\n";
         System.out.printf(format, ticket, last_digit, ticket_number, remainder, validity);  
+
+         try{
+             //read the file
+            File tickets = new File("tickets.txt");
+            //create a scanner to connect to the file
+            Scanner ticketsData = new Scanner(tickets);
+        
+            int linenum = 1;
+            //control the loop
+            while(ticketsData.hasNextLine()){
+                //reads one line of data
+                String ticketData = ticketsData.nextLine();
+                linenum++;
+
+                if (validity == true)
+                {
+                    //create the new file to write to
+                    try{
+                    FileWriter writeValid = new FileWriter("valid_tix.txt");
+                    writeValid.write(ticketData);
+                    writeValid.close();
+                    } //end try
+                    
+                    catch(IOException ex)
+                    {
+                        System.out.println("Unable to create file.");
+                    }
+
+                } else {
+                    //if not valid, move to next ticket number
+                    ticketsData.nextLine();
+                }
+                
+            } 
+            // close the scanner!!
+            ticketsData.close();
+        } //end try
+
+        catch(IOException ex){
+            System.out.println("File not found!");
+        } // end catch  
     }
 }
